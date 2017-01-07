7118b355bb01331b6936762561b07cd63b9d88bc
Eric Turner <turner.eric7@gmail.com>
2017-01-07 17:59:21 -0500

    Removed hard coded 'Sunny' forecast context. Enabled getWeather API function and added framework for future Google Maps Directions API calls

--- services/wit.js
+++ services/wit.js
@@ -78,25 +78,33 @@ var actions = {
 
 	// list of functions Wit.ai can execute
 	['fetch-weather'](sessionId, context, cb) {
-		// Here we can place an API call to a weather service
-		// if (context.loc) {
-		// 	getWeather(context.loc)
-		// 		.then(function (forecast) {
-		// 			context.forecast = forecast || 'sunny'
-		// 		})
-		// 		.catch(function (err) {
-		// 			console.log(err)
-		// 		})
-		// }
+		
+		if (context.loc) {
+			getWeather(context.loc)
+		 		.then(function (forecast) {
+		 			context.forecast = forecast
+		 		})
+		 		.catch(function (err) {
+		 			console.log(err)
+		 		})
+		}
 
-		context.forecast = 'Sunny'
 
 		cb(context)
 	},
 
-	['fetch-pics'](sessionId, context, cb) {
-		var wantedPics = allPics[context.cat || 'default']
-		context.pics = wantedPics[Math.floor(Math.random() * wantedPics.length)]
+	['fetch-directions'](sessionId, context, cb) {
+		
+		if (context.origin && context.dest) {
+			getDirections(context.origin, context.dest)
+		 		.then(function (forecast) {
+		 			context.directions = directions
+		 		})
+		 		.catch(function (err) {
+		 			console.log(err)
+		 		})
+		}
+
 
 		cb(context)
 	},
@@ -126,42 +134,26 @@ var getWeather = function (location) {
 		request(url, function (error, response, body) {
 		    if (!error && response.statusCode == 200) {
 		    	var jsonData = JSON.parse(body)
-		    	var forecast = jsonData.query.results.channel.item.forecast[0].text
-		      console.log('WEATHER API SAYS....', jsonData.query.results.channel.item.forecast[0].text)
-		      return forecast
+		    	var condition = jsonData.query.results.channel.item.condition[0].text
+			var temp = jsonData.query.results.channel.item.condition.temp[0].text
+		      console.log('WEATHER API SAYS.... CURRENTLY:', jsonData.query.results.channel.item.condition[0].text, 'TEMP: ',jsonData.query.results.channel.item.condition.temp[0].text)
+		      return [forecast,temp]
 		    }
 			})
 	})
 }
-
-// CHECK IF URL IS AN IMAGE FILE
-var checkURL = function (url) {
-    return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
-}
-
-// LIST OF ALL PICS
-var allPics = {
-  corgis: [
-    'http://i.imgur.com/uYyICl0.jpeg',
-    'http://i.imgur.com/useIJl6.jpeg',
-    'http://i.imgur.com/LD242xr.jpeg',
-    'http://i.imgur.com/Q7vn2vS.jpeg',
-    'http://i.imgur.com/ZTmF9jm.jpeg',
-    'http://i.imgur.com/jJlWH6x.jpeg',
-		'http://i.imgur.com/ZYUakqg.jpeg',
-		'http://i.imgur.com/RxoU9o9.jpeg',
-  ],
-  racoons: [
-    'http://i.imgur.com/zCC3npm.jpeg',
-    'http://i.imgur.com/OvxavBY.jpeg',
-    'http://i.imgur.com/Z6oAGRu.jpeg',
-		'http://i.imgur.com/uAlg8Hl.jpeg',
-		'http://i.imgur.com/q0O0xYm.jpeg',
-		'http://i.imgur.com/BrhxR5a.jpeg',
-		'http://i.imgur.com/05hlAWU.jpeg',
-		'http://i.imgur.com/HAeMnSq.jpeg',
-  ],
-  default: [
-    'http://blog.uprinting.com/wp-content/uploads/2011/09/Cute-Baby-Pictures-29.jpg',
-  ],
+// GET DIRECTIONS FROM API
+var getDirections = function (origin, dest) {
+	return new Promise(function (resolve, reject) {
+		var url = 'https://maps.googleapis.com/maps/api/directions/json?origin='+ origin + '&destination='+ destination + '4&key=AIzaSyB_oDSmms54zH3Ffb8hHv6854VcWCFKr24'
+		request(url, function (error, response, body) {
+		    if (!error && response.statusCode == 200) {
+		    	var jsonData = JSON.parse(body)
+		    	var directions = jsonData.query.routes.legs.steps[0].text
+		      console.log('DIRECTIONS API SAYS....', jsonData.query.routes.legs.steps[0].text)
+		      return directions
+		    }
+			})
+	})
 };
+
