66677a66152a39bb554ac62c7a2f818f4d64ff2d
Eric Turner <turner.eric7@gmail.com>
2017-01-07 18:06:45 -0500

    Modified getWeather and fetch-weather in attempts to pull current conditions and temperature data from yahoo api

--- services/wit.js
+++ services/wit.js
@@ -82,7 +82,9 @@ var actions = {
 		if (context.loc) {
 			getWeather(context.loc)
 		 		.then(function (forecast) {
-		 			context.forecast = forecast
+					var weather = weatherInfo()
+		 			context.forecast = weather[0]
+					context.temp = weather[1]
 		 		})
 		 		.catch(function (err) {
 		 			console.log(err)
@@ -133,11 +135,14 @@ var getWeather = function (location) {
 		var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ location +'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
 		request(url, function (error, response, body) {
 		    if (!error && response.statusCode == 200) {
-		    	var jsonData = JSON.parse(body)
-		    	var condition = jsonData.query.results.channel.item.condition[0].text
-			var temp = jsonData.query.results.channel.item.condition.temp[0].text
-		      console.log('WEATHER API SAYS.... CURRENTLY:', jsonData.query.results.channel.item.condition[0].text, 'TEMP: ',jsonData.query.results.channel.item.condition.temp[0].text)
-		      return [forecast,temp]
+			var weatherInfo = function(){
+		    		var jsonData = JSON.parse(body)
+		    		var condition = jsonData.query.results.channel.item.condition[0].text
+				var temp = jsonData.query.results.channel.item.condition.temp[0].text
+		      		console.log('WEATHER API SAYS.... CURRENTLY:', jsonData.query.results.channel.item.condition[0].text, 'TEMP: ',jsonData.query.results.channel.item.condition.temp[0].text)
+		      
+				return [condition,temp]
+			}
 		    }
 			})
 	})
