69bb545a2ddd06538edd826e63a65b8c0d7d98d2
Eric Turner <turner.eric7@gmail.com>
2017-01-07 23:39:33 -0500

    Still stumped with fetch-weather. Trial # 2

--- services/wit.js
+++ services/wit.js
@@ -45,7 +45,6 @@ var actions = {
 	merge(sessionId, context, entities, message, cb) {
 		// Reset the weather story
 		delete context.forecast
-		delete context.loc
 
 		// Retrive the location entity and store it in the context field
 		var loc = firstEntityValue(entities, 'location')
