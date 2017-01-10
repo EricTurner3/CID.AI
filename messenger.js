6a854f7af188b5911a3450fec2d3ea5c146e1a64
Eric Turner <turner.eric7@gmail.com>
2017-01-10 18:14:59 -0500

    Resolve Promises

--- messenger.js
+++ messenger.js
@@ -18,6 +18,7 @@ const crypto = require('crypto');
 const express = require('express');
 const fetch = require('node-fetch');
 const request = require('request');
+const rp = require('request-promise');
 
 var Config = require('./config')
 
@@ -157,6 +158,7 @@ const actions = {
 					console.log("fetchWeather: Setting context.forecast...");
 		 			context.forecast = forecast + " in " + loc + ".";
 					console.log("fetchWeather: context.forecast: " + context);
+					resolve(context);
 				})
 				.catch(function (err) {
 					console.log("fetchWeather: ERROR with getWeather(loc):");
@@ -168,33 +170,30 @@ const actions = {
 			context.missingLocation = true;
 			delete context.forecast;
 		}
-
-		resolve(context);
 	});
 },
 };
 
 // GET WEATHER FROM API
 
-var getWeather = new Promise(
-	function (resolve, reject) {
-		function (location){
-			console.log("fetchWeather: getWeather(): Accessing API to retrieve weather data...");
-			var url = 'http://api.openweathermap.org/data/2.5/find?q=' + location + '&units=imperial&appid=94f38a7a1a91948b0e04e86d5d4d2ef3'
-			request(url, function (error, response, body) {
-				if (!error && response.statusCode == 200) {
-					console.log("fetchWeather: getWeather(): API reached, inputting location and waiting for results...");
-					var jsonData = JSON.parse(body);
-					//var condition = jsonData.list[0].weather[0].main
-					//var temp = jsonData.list[0].main.temp
-					var forecast = jsonData.list[0].weather[0].main + " with a temperature of " + jsonData.list[0].main.temp + " degrees";
-					console.log('fetchWeather: getWeather(): WEATHER API SAYS.... ', forecast)
-					resolve(forecast);
-				}
-			})
-		}
-	});
 
+var getWeather = function (location) {
+	return new Promise(function (resolve, reject) {
+		console.log("fetchWeather: Accessing API to retrieve weather data...");
+		var url = 'http://api.openweathermap.org/data/2.5/find?q=' + location + '&units=imperial&appid=94f38a7a1a91948b0e04e86d5d4d2ef3'
+		request(url, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				console.log("fetchWeather: API reached, inputting location and waiting for results...");
+				var jsonData = JSON.parse(body);
+				//var condition = jsonData.list[0].weather[0].main
+				//var temp = jsonData.list[0].main.temp
+				var forecast = jsonData.list[0].weather[0].main + " with a temperature of " + jsonData.list[0].main.temp + " degrees";
+				console.log('fetchWeather: WEATHER API SAYS.... ', jsonData.list[0].weather[0].main + " with a temperature of " + jsonData.list[0].main.temp + " degrees")
+				resolve(forecast);
+			}
+		})
+	});
+}
 
 
 // Setting up our bot
